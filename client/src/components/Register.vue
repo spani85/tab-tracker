5c73e89aee1bc0eaf9b66cca2d3724eb5eba647d
spani85 <stefano.panipucci@gmail.com>
2017-09-10 23:59:15 +0200

    add user model; add some front styling

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,22 +1,31 @@
 <template>
-  <div>
-    <h1>Register</h1>
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
 
-    <input 
-      type="email" 
-      name="email" 
-      v-model="email"
-      placeholder="email">
-    <br>
-    <input 
-      type="password" 
-      name="password" 
-      v-model="password"
-      placeholder="password">
-    <br>
-    <button @click="register">Register</button>
-
-  </div>
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <input 
+            type="email" 
+            name="email" 
+            v-model="email"
+            placeholder="email">
+          <br>
+          <input 
+            type="password" 
+            name="password" 
+            v-model="password"
+            placeholder="password">
+          <br>
+          <div class="error" v-html="error"></div>
+          <br>
+          <v-btn class="cyan" @click="register">Register</v-btn>
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -25,17 +34,27 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: null
     }
   },
   methods: {
     async register () {
-      const response = await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
-      console.log(response.data)
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
 </script>
+
+<style scoped>
+  .error {
+    color: red;
+  }
+</style>
